76503ae690c9de35554453bcf6e44d88366053f6
hitomi09 <86147871+hitomi09@users.noreply.github.com>
2021-06-19 17:57:03 +0900

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,29 @@
+from flask important Flask
+
+application = Flask(__name__)
+
+aisatsu = input()
+if aisatsu == "good morning":
+  print("おはよう")
+if aisatsu == "Good morning":
+  print("おはよう") 
+if aisatsu == "Good Morning":
+  print("おはよう") 
+if aisatsu == "GOOD MORNING":
+  print("おはよう") 
+if aisatsu == "hello":
+  print("こんにちは")
+if aisatsu == "Hello":
+  print("こんにちは")
+if aisatsu == "HELLO":
+  print("こんにちは")
+if aisatsu == "good evening":
+  print("こんばんは")
+if aisatsu == "Good evening":
+  print("こんばんは")
+if aisatsu == "Good Evening":
+  print("こんばんは")
+if aisatsu == "GOOD EVENING":
+  print("こんばんは")
+else:
+  print("わかりません")
