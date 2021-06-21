7359609ec4e49c2acae4a619b6cd615b595a635b
nanai <86147871+nanai0929@users.noreply.github.com>
2021-06-21 22:19:21 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -14,32 +14,3 @@ def root():
 </body></html>
 """
     return s
-
-@app.route('/gree', methods=["post"])
-def gree():
-    a = str(request.form.get("a"))
-    {% if a == "good morning" %}
-      ret = "おはよう"
-    {% if a == "Good morning" %}
-      ret = "おはよう"
-    {% if a == "Good Morning" %}
-      ret = "おはよう"
-    {% if a == "GOOD MORNING" %}
-      ret = "おはよう"
-    {% if a == "hello" %}
-      ret = "こんにちは"
-    {% if a == "Hello" %}
-      ret = "こんにちは"
-    {% if a == "HELLO" %}
-      ret = "こんにちは"
-    {% if a == "good evening" %}
-      ret = "こんばんは"
-    {% if a == "Good evening" %}
-      ret = "こんばんは"
-    {% if a == "Good Evening" %}
-      ret = "こんにちは"
-    {% if a == "GOOD EVENING" %}
-      ret = "こんにちは"
-    {% else %}
-      ret = "わかりません"
-    return "{}。".format(ret)
