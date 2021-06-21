1735bd71ed5da08fc7bc85f46c98664880f719f7
nanai <86147871+nanai0929@users.noreply.github.com>
2021-06-21 22:41:44 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -5,15 +5,12 @@ application = Flask(__name__)
 
 @application.route('/')
 def root():
-    s = """
 <html><body>
 <form action="/gree" method="post">
  <input type="text" name="a">
  <input type="submit" value="入力">
 </form>
 </body></html>
-"""
-    return s
 
 @application.route('/gree', methods=["post"])
 def gree():
