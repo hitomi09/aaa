d990b0eb3973e405763907f4bf66a9aaee279d9a
nanai <86147871+nanai0929@users.noreply.github.com>
2021-06-21 22:31:14 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -4,22 +4,42 @@ from flask import request
 application = Flask(__name__)
 
 @application.route('/')
-def index():
+def root():
     s = """
 <html><body>
-<form action="/calc" method="post">
- <input type="text" name="a">x
- <input type="text" name="b">
- <input type="submit" value="計算">
+<form action="/gree" method="post">
+ <input type="text" name="a">
+ <input type="submit" value="入力">
 </form>
 </body></html>
 """
     return s
 
-# フォームの処理
-@application.route('/calc', methods=["post"])
-def calc():
-    a = int(request.form.get("a"))
-    b = int(request.form.get("b"))
-    ret = a * b
-    return "<H1>掛け算の結果は {}".format(ret)
+@application.route('/gree', methods=["post"])
+def gree():
+    a = str(request.form.get("a"))
+    {% if a == "good morning" %}
+      ret = "おはよう"
+    {% if a == "Good morning" %}
+      ret = "おはよう"
+    {% if a == "Good Morning" %}
+      ret = "おはよう"
+    {% if a == "GOOD MORNING" %}
+      ret = "おはよう"
+    {% if a == "hello" %}
+      ret = "こんにちは"
+    {% if a == "Hello" %}
+      ret = "こんにちは"
+    {% if a == "HELLO" %}
+      ret = "こんにちは"
+    {% if a == "good evening" %}
+      ret = "こんばんは"
+    {% if a == "Good evening" %}
+      ret = "こんばんは"
+    {% if a == "Good Evening" %}
+      ret = "こんにちは"
+    {% if a == "GOOD EVENING" %}
+      ret = "こんにちは"
+    {% else %}
+      ret = "わかりません"
+    return "{}。".format(ret)
