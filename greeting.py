7c77292c18b6af3c70ff72792000265e1b69a591
nanai <86147871+nanai0929@users.noreply.github.com>
2021-06-21 22:24:09 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -3,14 +3,23 @@ from flask import request
 
 application = Flask(__name__)
 
-@app.route('/')
-def root():
+@application.route('/')
+def index():
     s = """
 <html><body>
-<form action="/gree" method="post">
- <input type="text" name="a">
- <input type="submit" value="入力">
+<form action="/calc" method="post">
+ <input type="text" name="a">x
+ <input type="text" name="b">
+ <input type="submit" value="計算">
 </form>
 </body></html>
 """
     return s
+
+# フォームの処理
+@application.route('/calc', methods=["post"])
+def calc():
+    a = int(request.form.get("a"))
+    b = int(request.form.get("b"))
+    ret = a * b
+    return "<H1>掛け算の結果は {}".format(ret)
